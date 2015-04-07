581c3e6da4948f7cc1da53c206854b9d5aa3f7f8
Aaron Harrington <aharrington@aviatainc.com>
2015-04-07 11:32:01 -0600

    All bugs seem to be fixed, copied installation from seperate docker file to nimbus and supervisor, I believe that is what is needed, files need to be copied to those folders as well

--- config-supervisord.sh
+++ config-supervisord.sh
@@ -1,5 +1,12 @@
-echo [program:storm-$1] | tee -a /etc/supervisor/conf.d/storm-$1.conf
-echo command=storm $1 | tee -a /etc/supervisor/conf.d/storm-$1.conf
-echo directory=/home/storm | tee -a /etc/supervisor/conf.d/storm-$1.conf
-echo autorestart=true | tee -a /etc/supervisor/conf.d/storm-$1.conf
-echo user=root | tee -a /etc/supervisor/conf.d/storm-$1.conf
+#file does not exist
+#echo [program:storm-$1] | tee -a /etc/supervisor/conf.d/storm-$1.conf
+#echo command=storm $1 | tee -a /etc/supervisor/conf.d/storm-$1.conf
+#echo directory=/home/storm | tee -a /etc/supervisor/conf.d/storm-$1.conf
+#echo autorestart=true | tee -a /etc/supervisor/conf.d/storm-$1.conf
+#echo user=root | tee -a /etc/supervisor/conf.d/storm-$1.conf
+
+echo [program:storm-$1] | tee -a /etc/supervisor/config-$1.conf
+echo command=storm $1 | tee -a /etc/supervisor/config-$1.conf
+echo directory=/home/storm | tee -a /etc/supervisor/config-$1.conf
+echo autorestart=true | tee -a /etc/supervisor/config-$1.conf
+echo user=root | tee -a /etc/supervisor/config-$1.conf
