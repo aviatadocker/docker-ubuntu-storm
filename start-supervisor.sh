9efedf2a2a395e28fccddc937a9bfb7371c6cc2c
aharringtonaviata <aharrington@aviatainc.com>
2015-04-13 13:57:19 -0600

    Updated start-supervisor.sh
    It now contains a command that actually starts storm.

--- start-supervisor.sh
+++ start-supervisor.sh
@@ -1,4 +1,7 @@
 sed -i -e "s/%zookeeper%/$ZK_PORT_2181_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
 sed -i -e "s/%nimbus%/$NIMBUS_PORT_6627_TCP_ADDR/g" $STORM_HOME/conf/storm.yaml
 echo "storm.local.hostname: `hostname -i`" >> $STORM_HOME/conf/storm.yaml
-/usr/sbin/sshd && supervisord
+# I'm not sure what this line was for
+#/usr/sbin/sshd && supervisord
+#$1 should be storm or nimbus
+/usr/bin/storm $1
